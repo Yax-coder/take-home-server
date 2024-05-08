bb62791f2ccf6f5ec93b13ad545a395ee3a16231
Yahya <yahay.me@gmail.com>
2024-05-08 15:09:19 +0100

    added a start command

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
