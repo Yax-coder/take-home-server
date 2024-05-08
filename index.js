4b5935467021e20ac04d8e43e9efd988e8ce7adf
Yahya <yahay.me@gmail.com>
2024-05-08 17:20:20 +0100

    updated routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute");
+const messageRoutes = require('./Routes/messagesRoute');
 
 const app = express();
 require("dotenv").config();
@@ -9,13 +10,13 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
+app.use('/api/messages', messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.port || 5000;
 const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
@@ -23,8 +24,6 @@ app.listen(port, (req, res) => {
 });
 
 mongoose
-  .connect(
-    'mongodb+srv://yahayme:passwordeng@cluster0.fjzmlhz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-  )
+  .connect(uri)
   .then(() => console.log("MongoDB connection established"))
   .catch((err) => console.log("MongoDB connection failed", err.message));
